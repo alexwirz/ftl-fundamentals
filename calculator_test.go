ca67d854c53524ac3abb569bb45b3c272353d4e9
Alexander Wirz <alexwirz@gmail.com>
2020-11-18 21:03:38 +0100

    multiply

--- calculator_test.go
+++ calculator_test.go
@@ -22,3 +22,21 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("want %f, got %f", want, got)
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 6
+	got := calculator.Multiply(2, 3)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestMultiplyThreeAndThree(t *testing.T) {
+	t.Parallel()
+	var want float64 = 9
+	got := calculator.Multiply(3, 3)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
